f96f2d89a67eccbf097ffebf073120aef4f4619e
Alexander Boriskin <Alexander.Boriskin@dataart.com>
2018-02-03 17:29:16 +0300

    Initialize IFrame and bind it to 3 text areas: html, css and js

---
+++ WarmingUp/WebSite/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebSite.Models;
+
+namespace WebSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Blank()
+        {
+            return View();
+        }        
+    }
+}
