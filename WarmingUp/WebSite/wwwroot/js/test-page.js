01b0938dc553dcedfc5a7b9c3d0de29b20530e97
Alexander Boriskin <Alexander.Boriskin@dataart.com>
2018-02-05 12:25:28 +0300

    Integrate with JQuery ui tabs and ACE Emmet plugin

---
+++ WarmingUp/WebSite/wwwroot/js/test-page.js
@@ -0,0 +1,46 @@
+﻿'use strict';
+
+(function () {
+    var htmlEditor, cssEditor, jsEditor;
+
+    $(function () {
+        htmlEditor = initEditor("html", true);
+        cssEditor = initEditor("css");
+        jsEditor = initEditor("javascript");
+
+        $("#tabs").tabs();
+    });
+
+    function initEditor(language, enableEmmet) {        
+        var editor = ace.edit(`area-${language}`);
+        var mode = 'ace/mode/' + language;
+            editor.session.setMode(mode);
+
+        if (enableEmmet) {                
+            editor.setOption("enableEmmet", true);
+        }            
+
+        editor.getSession().on('change', _.debounce(updateContentIFrame, 1000));
+        return editor;        
+    }
+
+    function updateContentIFrame() {
+        var html = htmlEditor.getValue();
+        var css = cssEditor.getValue();
+        var js = jsEditor.getValue();
+
+        var result = 
+`<head>
+<meta name="viewport" content="width=device-width" />
+<title>IFrame content</title>
+<style>${css}</style>
+</head>
+<body>
+${html}
+<script>${js}</scr` + `ipt>
+</body>`;
+        $("#frame-result").contents().find("html")[0].innerHTML = result;                
+        frames[0].window.eval(js);
+    }
+
+}());
